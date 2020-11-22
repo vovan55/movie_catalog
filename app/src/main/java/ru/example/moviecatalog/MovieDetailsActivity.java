98fa018e13506c1aa4fc0206a39bde919ce0251c
vovan55 <mr.vovan55@gmail.com>
2020-11-22 23:01:13 +0600

    без звезд.

---
+++ app/src/main/java/ru/example/moviecatalog/MovieDetailsActivity.java
@@ -0,0 +1,14 @@
+package ru.example.moviecatalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+    }
+}
