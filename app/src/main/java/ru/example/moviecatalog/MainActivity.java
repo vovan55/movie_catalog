76c8bb5ad0510c76f71516d2d2a699481b36672d
vovan55 <mr.vovan55@gmail.com>
2020-11-23 00:51:51 +0600

    done

--- app/src/main/java/ru/example/moviecatalog/MainActivity.java
+++ app/src/main/java/ru/example/moviecatalog/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
